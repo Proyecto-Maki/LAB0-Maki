fff86b9b737aaba8b5f34251bfaa391b736d351a
Kelly Solano <kelly.solano.1403@gmail.com>
2024-12-14 16:31:12 -0500

    Creacion de modelo departamento

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,14 +3,6 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
-class Region(models.Model):
-    id_region = models.AutoField(primary_key=True)
-    nombre_region = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre_region
-
 class Persona(models.Model):
     id_persona = models.CharField(max_length=10, primary_key=True)
     nombre_1_persona = models.CharField(max_length=50)
@@ -25,6 +17,26 @@ class Persona(models.Model):
         'self', on_delete=models.SET_NULL, blank=True, null=True, related_name='miembros_familia')
     def __str__(self):
         return f"{self.id_persona} {self.nombre_1_persona} {self.apellido_1_persona}"
+class Region(models.Model):
+    id_region = models.AutoField(primary_key=True)
+    nombre_region = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_region
+    
+class Departamento(models.Model):
+    id_departamento = models.AutoField(primary_key=True)
+    nombre_departamento = models.CharField(max_length=50)
+    id_region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    id_gobernador = models.ForeignKey(Persona, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nombre_departamento
+    
+
+
+
     
 
     
