5298a7c76a05f82bd2fe09f870ad61015ce592f8
Kelly Solano <kelly.solano.1403@gmail.com>
2024-12-14 20:57:27 -0500

    Actualizacion de frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import Form from '../components/Form'
+import '../styles/Base.css'
 
 function Login () {
     return (
-        <Form route="/api/token/" method="login"/>
+        <div className='container'>
+            <div className='container-title'>
+                <h1>Login</h1>
+            </div>
+            <div className='container-form'>
+                <Form route="/api/token/" method="login"/>
+            </div>
+            
+        </div>
+        
     );
 }
 
