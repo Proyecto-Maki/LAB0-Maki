04438379a86c4c59192c870d522e82095468f3b9
Kelly Solano <kelly.solano.1403@gmail.com>
2024-12-15 00:08:29 -0500

    Login realizado

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,23 @@
 import Form from '../components/Form'
 import '../styles/Base.css'
+import { useNavigate } from 'react-router-dom';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login () {
+
+    const navigate = useNavigate();
+
+    const register = () => {
+        navigate('/register');
+    }
+
     return (
         <div className='container'>
             <div className='container-title'>
                 <div className='container-title-til'>¡Bienvenido!</div>
                 <div className='container-title-sub-1'>Inicia sesión para continuar</div>
                 <div className='container-title-sub-2'>¿No tienes cuenta?</div>
-                <button className='container-title-button'>Regístrate</button>
+                <button className='container-title-button' onClick={register}>Regístrate</button>
             </div>
             <div className='container-form'>
                 <Form route="/api/token/" method="login"/>
