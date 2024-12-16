5255f6bdbfcb31617cd0ec3604882ddd60d648b7
Kelly Solano <kelly.solano.1403@gmail.com>
2024-12-15 19:36:08 -0500

    Correccion de nulidad en modelos

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Persona(models.Model):
-    id_persona = models.CharField(max_length=10, primary_key=True)
-    nombre_1_persona = models.CharField(max_length=50)
+    id_persona = models.CharField(max_length=10, primary_key=True, null=False)
+    nombre_1_persona = models.CharField(max_length=50, null=False)
     nombre_2_persona = models.CharField(max_length=50, blank=True, null=True)
-    apellido_1_persona = models.CharField(max_length=50)
+    apellido_1_persona = models.CharField(max_length=50, null=False)    
     apellido_2_persona = models.CharField(max_length=50, blank=True, null=True)
     fecha_nacimiento = models.DateField(null=False)
     genero_persona = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro')], null=False)
@@ -19,16 +19,16 @@ class Persona(models.Model):
         return f"{self.id_persona} {self.nombre_1_persona} {self.apellido_1_persona}"
 class Region(models.Model):
     id_region = models.AutoField(primary_key=True)
-    nombre_region = models.CharField(max_length=50)
+    nombre_region = models.CharField(max_length=50, null=False)
 
     def __str__(self):
         return self.nombre_region
     
 class Departamento(models.Model):
     id_departamento = models.AutoField(primary_key=True)
-    nombre_departamento = models.CharField(max_length=50)
-    id_region = models.ForeignKey(Region, on_delete=models.CASCADE)
-    id_gobernador = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    nombre_departamento = models.CharField(max_length=50, null=False)
+    id_region = models.ForeignKey(Region, on_delete=models.CASCADE, null=False)
+    id_gobernador = models.ForeignKey(Persona, on_delete=models.CASCADE, null=True)
 
 
     def __str__(self):
@@ -37,9 +37,9 @@ class Departamento(models.Model):
 
 class Municipio(models.Model):
     id_municipio = models.AutoField(primary_key=True)
-    nombre_municipio = models.CharField(max_length=50)
-    id_departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    id_alcalde = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    nombre_municipio = models.CharField(max_length=50, null=False)
+    id_departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE, null=False)
+    id_alcalde = models.ForeignKey(Persona, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.nombre_municipio
@@ -47,10 +47,10 @@ class Municipio(models.Model):
 
 class Vivienda(models.Model):
     id_vivienda = models.AutoField(primary_key=True)
-    direccion_vivienda = models.CharField(max_length=100)
-    estrato_vivienda = models.IntegerField()
-    id_municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE)
-    id_persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    direccion_vivienda = models.CharField(max_length=100, null=False)
+    estrato_vivienda = models.IntegerField(null=False)
+    id_municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE, null=False)
+    id_persona = models.ForeignKey(Persona, on_delete=models.CASCADE, null=False)   
 
     def __str__(self):
         return self.direccion_vivienda
@@ -58,10 +58,10 @@ class Vivienda(models.Model):
 
 class Mascota(models.Model):
     id_mascota = models.AutoField(primary_key=True)
-    nombre_mascota = models.CharField(max_length=50)
-    especie_mascota = models.CharField(max_length=50)
-    raza_mascota = models.CharField(max_length=50)
-    id_vivienda = models.ForeignKey(Vivienda, on_delete=models.CASCADE)
+    nombre_mascota = models.CharField(max_length=50, null=False)
+    especie_mascota = models.CharField(max_length=50, null=False)
+    raza_mascota = models.CharField(max_length=50, null=False)
+    id_vivienda = models.ForeignKey(Vivienda, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.nombre_mascota
