8e4a402caf3628b9906c1c4806650d112390f28b
Kelly Solano <kelly.solano.1403@gmail.com>
2024-12-16 16:37:23 -0500

    Creación de rutas en el backend para regiones, departamentos, municipios

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,15 @@ urlpatterns = [
     path('personas/mayores_edad/', views.PersonaMayoresEdad.as_view(), name="mayores_edad"),
     path('personas/delete/<str:pk>/', views.PersonaDelete.as_view(), name="eliminar_persona"),
     path('personas/update/<str:pk>/', views.PersonaUpdate.as_view(), name="actualizar_persona"),
+    path('regiones/', views.RegionListCreate.as_view(), name="regiones"),
+    path('regiones/delete/<int:id>', views.RegionDelete.as_view(), name="eliminar_region"),
+    path('regiones/update/<int:id>', views.RegionUpdate.as_view(), name="actualizar_region"),
+    path('departamentos/', views.DepartamentoListCreate.as_view(), name="departamentos"),
+    path('departamentos/delete/<int:id>', views.DepartamentoDelete.as_view(), name="eliminar_departamento"),
+    path('departamentos/update/<int:id>', views.DepartamentoUpdate.as_view(), name="actualizar_departamento"),
+    path('municipios/', views.MunicipioListCreate.as_view(), name="municipios"),
+    path('municipios/delete/<int:id>', views.MunicipioDelete.as_view(), name="eliminar_municipio"),
+    path('municipios/update/<int:id>', views.MunicipioUpdate.as_view(), name="actualizar_municipio"),
+
     
 ]
