60c3983dbe4b8bad6937d5c856fd8222d3922ca0
Kelly Solano <kelly.solano.1403@gmail.com>
2024-12-17 16:31:43 -0500

    Progreso en crud vivienda

--- frontend/src/components/Vivienda/ViviendaForm.jsx
+++ frontend/src/components/Vivienda/ViviendaForm.jsx
@@ -1,4 +1,85 @@
-function ViviendaForm(){
+import { useState } from 'react';
+import api from '../../api';
+import { useNavigate } from 'react-router-dom';
+import '../../styles/Vivienda/ViviendaForm.css';
+
+function ViviendaForm({get_viviendas, get_personas, get_municipios}){
+    const [id_vivienda, set_id_vivienda] = useState("");
+    const [direccion_vivienda, set_direccion_vivienda] = useState("");
+    const [estrato_vivienda, set_estrato_vivienda] = useState("");
+    const [id_municipio, set_id_municipio] = useState("");
+    const [id_persona, set_id_persona] = useState("");
+
+    const navigate = useNavigate();
+
+    const create_vivienda = async (e) => {
+        e.preventDefault();
+        const nueva_vivienda = {
+            id_vivienda: id_vivienda,
+            direccion_vivienda: direccion_vivienda,
+            estrato_vivienda: estrato_vivienda,
+            id_municipio: id_municipio,
+            id_persona: id_persona
+        };
+        console.log(nueva_vivienda);
+
+        api
+            .post("/api/viviendas/", nueva_vivienda)
+            .then((res) => {
+                if (res.status === 201) {
+                    alert("Vivienda creada");
+                } else {
+                    alert("Error al crear la vivienda");
+                }
+                get_viviendas();
+                set_id_vivienda("");
+                set_direccion_vivienda("");
+                set_estrato_vivienda("");
+                set_id_municipio("");
+                set_id_persona("");
+            })
+            .catch((err) => {
+                console.log(err.response.data);
+                alert("Error al crear la vivienda");
+            });
+    };
+
+    return(
+        <form className="form-container-vivienda" onSubmit={create_vivienda}>
+            <h2>Crear Vivienda</h2>
+            <div className="form-group-vivienda">
+                <label htmlFor="id_vivienda">Id Vivienda</label>
+                <input type="text" id="id_vivienda" value={id_vivienda} onChange={(e) => set_id_vivienda(e.target.value)} required/>
+            </div>
+            <div className="form-group-vivienda">
+                <label htmlFor="direccion_vivienda">Dirección</label>
+                <input type="text" id="direccion_vivienda" value={direccion_vivienda} onChange={(e) => set_direccion_vivienda(e.target.value)} required/>
+            </div>
+            <div className="form-group-vivienda">
+                <label htmlFor="estrato_vivienda">Estrato</label>
+                <input type="text" id="estrato_vivienda" value={estrato_vivienda} onChange={(e) => set_estrato_vivienda(e.target.value)} required/>
+            </div>
+            <div className="form-group-vivienda">
+                <label htmlFor="id_municipio">Municipio</label>
+                <select id="id_municipio" value={id_municipio} onChange={(e) => set_id_municipio(e.target.value)} required>
+                    <option value="">Seleccione un municipio</option>
+                    {get_municipios().map((municipio) => (
+                        <option key={municipio.id_municipio} value={municipio.id_municipio}>{municipio.nombre_municipio}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="form-group-vivienda">
+                <label htmlFor="id_persona">Propietario(a)</label>
+                <select id="id_persona" value={id_persona} onChange={(e) => set_id_persona(e.target.value)} required>
+                    <option value="">Seleccione una persona</option>
+                    {get_personas().map((persona) => (
+                        <option key={persona.id_persona} value={persona.id_persona}>{persona.id_persona} {persona.nombre_1_persona} {persona.apellido_1_persona}</option>
+                    ))}
+                </select>
+            </div>
+            <button type="submit" className="form-button-vivienda">Crear vivienda</button>
+        </form>
+    );
 
 }
 
--- frontend/src/components/Vivienda/ViviendaFormEdit.jsx
+++ frontend/src/components/Vivienda/ViviendaFormEdit.jsx
@@ -1,8 +1,77 @@
-function ViviendaFormEdit(){
-    return (
-        <div>
-            <h1>Editar vivienda</h1>
-            <ViviendaForm />
+import { useState, useEffect } from "react";
+import api from "../../api";
+import "../../styles/Persona/ModalEditarVivienda.css";
+
+function ViviendaFormEdit({isEditarOpen, cerrarEditar, vivienda, get_viviendas, get_personas, get_municipios}){
+    const [id_vivienda, set_id_vivienda] = useState(vivienda.id_vivienda);
+    const [direccion_vivienda, set_direccion_vivienda] = useState(vivienda.direccion_vivienda);
+    const [estrato_vivienda, set_estrato_vivienda] = useState(vivienda.estrato_vivienda);
+    const [id_municipio, set_id_municipio] = useState(vivienda.id_municipio);
+    const [id_persona, set_id_persona] = useState(vivienda.id_persona);
+
+    const editarVivienda = async (e) => {
+        e.preventDefault();
+
+        const updated_vivienda = {
+            id_vivienda,
+            direccion_vivienda,
+            estrato_vivienda,
+            id_municipio,
+            id_persona
+        };
+
+        console.log(updated_vivienda);
+
+        try {
+            const res = await api.put(`/api/viviendas/update/${id_vivienda}/`, updated_vivienda);
+            if (res.status === 200) {
+                alert("Vivienda actualizada");
+                cerrarEditar(); // Cierra el modal
+            } else {
+                alert("Error al actualizar la vivienda");
+            }
+        } catch (err) {
+            console.log(err.response.data);
+            alert("Error al actualizar la vivienda");
+        }
+    }
+
+    return(
+        <div className="modal-editar-vivienda">
+            <div className="modal-editar-vivienda-content">
+                <span className="close" onClick={cerrarEditar}>&times;</span>
+                <form onSubmit={editarVivienda} className='form-container-vivienda'>
+                    <h2 className='form-container-h2'>Editar Vivienda</h2>
+                    <div className='form-container-agr'>
+                        <label htmlFor="id_vivienda">ID</label>
+                        <input type="text" id="id_vivienda" value={id_vivienda} onChange={(e) => set_id_vivienda(e.target.value)} disabled />
+                    </div>
+                    <div className='form-container-agr'>
+                        <label htmlFor="direccion_vivienda">Dirección</label>
+                        <input type="text" id="direccion_vivienda" value={direccion_vivienda} onChange={(e) => set_direccion_vivienda(e.target.value)} disabled/>
+                    </div>
+                    <div className='form-container-agr'>
+                        <label htmlFor="estrato_vivienda">Estrato</label>
+                        <input type="text" id="estrato_vivienda" value={estrato_vivienda} onChange={(e) => set_estrato_vivienda(e.target.value)} />
+                    </div>
+                    <div className='form-container-agr'>
+                        <label htmlFor="id_municipio">Municipio</label>
+                        <input type="text" id="id_municipio" value={id_municipio} onChange={(e) => set_id_municipio(e.target.value)} disabled/>
+                    </div>
+                    <div className='form-container-agr'>
+                        <label htmlFor="id_persona">Propietario(a)</label>
+                        <select id="id_persona" value={id_persona} onChange={(e) => set_id_persona(e.target.value)} required>
+                            <option value="">Seleccione una opción</option>
+                            {
+                                get_personas.map((persona) => (
+                                    <option key={persona.id_persona} value={persona.id_persona}>{persona.id_persona} {persona.nombre_1_persona} {persona.apellido_1_persona}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <button type="submit" className='form-container-button'>Actualizar persona</button>
+                </form>
+            </div>
         </div>
     );
 }
